93cd42e8cebadc9754a2acfd2688b3a12940f2fb
Vanya Pavlov <ivan.pavlov@jetbrains.com>
2022-12-01 10:50:02 +0200

    Day one solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,27 @@
 fun main() {
+    fun elfWeights(input: List<String>): MutableList<Int> {
+        val elfs = mutableListOf<Int>()
+        elfs.add(0)
+        input.forEach {
+            if (it == "") {
+                elfs.add(0)
+            } else {
+                elfs[elfs.size - 1] += it.toInt()
+            }
+        }
+        return elfs
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val elfs = elfWeights(input)
+        return elfs.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val elfs = elfWeights(input)
+        elfs.sort()
+        return elfs.takeLast(3).sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
